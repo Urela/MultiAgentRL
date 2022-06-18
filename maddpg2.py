0f21ed806366bbdb127ec2fc3e923f1c8b9e4d63
Urela <93521654+Urela@users.noreply.github.com>
2022-06-18 15:54:35 +0200

    Add files via upload

--- maddpg2.py
+++ maddpg2.py
@@ -10,6 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 gamma = 0.99
+tau   = 0.005 # for target network soft update
+
 class ReplayBuffer():
   def __init__(self, size=50000):
     self.memory = collections.deque(maxlen=size)
@@ -25,7 +27,7 @@ class ReplayBuffer():
     actions = torch.tensor([x[1] for x in batch]).float()
     rewards = torch.tensor([[x[2]] for x in batch]).float()
     nstates = torch.tensor([x[3] for x in batch], dtype=torch.float)
-    dones   = torch.tensor([1-x[4] for x in batch])
+    dones   = torch.tensor([[1-x[4]] for x in batch])
     return states, actions, rewards, nstates, dones
 
 class Actor(nn.Module):
@@ -91,45 +93,44 @@ class MADDPG():
     return actions
 
   def train(self):
-    for i, a in enumerate(self.agents):  
+    for a in self.agents:  
       if(len(self.agent_memorys[a]) >= 2000):
         #print("learning")
         states, actions, rewards, nstates, dones = self.agent_memorys[a].sample(32)
-        #print(type(states), type(actions))
 
         # RuntimeError: mat1 and mat2 shapes cannot be multiplied (32x9 and 13x128)
         #q = self.agents[a].critic(list(states.values()), list(actions.values()))
         #print("world state",torch.cat([states, actions],dim=1).shape)
-
         #print( torch.cat([states, actions], dim=1).shape, states.shape, actions.shape )
 
         q = self.agents[a].critic(states, actions)
-        #q = self.agents[a].critic(states, actions)
         a_targ = self.agents[a].targ_actor(nstates)
         q_targ = self.agents[a].targ_critic(nstates, a_targ)
+        #print((rewards + gamma*q_targ * dones).shape, (gamma*q_targ*dones).shape, (q_targ*dones).shape, q_targ.shape)
         q_targ = rewards + gamma*q_targ * dones
+
         critic_loss = F.smooth_l1_loss(q, q_targ.detach() )
 
         self.agents[a].critic.optimizer.zero_grad()
         critic_loss.backward()
         self.agents[a].critic.optimizer.step()
 
-        #a_pred = self.agents.actor(states)
-        #actor_loss = self.agents[a].critic(states, a_pred).mean()
-        #self.agents[a].actor.optimizer.zero_grad()
-        #actor_loss.backward()
-        #self.agents[a].actor.optimizer.step()
+        a_pred = self.agents[a].actor(states)
+        actor_loss = self.agents[a].critic(states, a_pred).mean()
+        self.agents[a].actor.optimizer.zero_grad()
+        actor_loss.backward()
+        self.agents[a].actor.optimizer.step()
 
-    #self.update_targets()
+    self.update_targets()
 
   def update_targets(self):
       # soft update https://github.com/seungeunrho/minimalRL/blob/master/ddpg.py
-    for i, agent in enumerate(self.agents):  
-      network, target = agent.actor, agent.targ_actor
+    for a in self.agents:  
+      network, target = self.agents[a].actor, self.agents[a].targ_actor
       for param_targ, param in zip(target.parameters(), network.parameters()):
         param_targ.data.copy_(param_targ.data * (1.0 - tau) + param.data * tau)
 
-      network, target = agent.critic, agent.targ_critic
+      network, target = self.agents[a].critic, self.agents[a].targ_critic
       for param_targ, param in zip(target.parameters(), network.parameters()):
         param_targ.data.copy_(param_targ.data * (1.0 - tau) + param.data * tau)
 
@@ -140,7 +141,16 @@ env = simple_adversary_v2.parallel_env(max_cycles=25)
 env.reset()
 AGENT = MADDPG(env)
 
-episode_num = 30000
+#episode_num = 30000
+episode_num = 10000
+episode_num = 10000
+num_agents = env.num_agents
+print( num_agents, "number of agents")
+scores = {agent: np.zeros(episode_num) for agent in env.agents} # reward of each episode of each agent
+
+steps = 0  # global step counter
+for episode in range(episode_num):
+  obs = env.reset()
 num_agents = env.num_agents
 print( num_agents, "number of agents")
 scores = {agent: np.zeros(episode_num) for agent in env.agents} # reward of each episode of each agent
@@ -153,6 +163,7 @@ for episode in range(episode_num):
     steps += 1
     #action = {a: env.action_space(a).sample() for a in env.agents}
     action = AGENT.get_action(obs) 
+    #env.render()
     next_obs, reward, done, info = env.step(action)
     for agent_id in obs.keys():
       _state  = obs[agent_id]
@@ -165,7 +176,27 @@ for episode in range(episode_num):
 
     obs = next_obs
     AGENT.train()
-    #print("running")
-    #env.render()
-    for a, r in reward.items():  # update reward
+
+    for a, r in reward.items():  # update episodic reward
       agent_score[a] += r
+
+    # episode finishes
+  for a, r in agent_score.items():  # record reward
+      scores[a][episode] = r
+
+  if (episode + 1) % 100 == 0:  # print info every 100 episodes
+    message = f'episode {episode + 1}, '
+    sum_reward = 0
+    for agent_id, r in agent_score.items():  # record reward
+      message += f'{agent_id}: {r:>4f}; '
+      sum_reward += r
+    message += f'sum reward: {sum_reward}'
+    print(message)
+
+env.close()
+
+from bokeh.plotting import figure, show
+for a in scores:
+  p = figure(title="TODO", x_axis_label="Episodes", y_axis_label="Scores")
+  p.line(np.arange(len(scores[a])), scores[a],  legend_label=str(a),  line_width=2)
+show(p) 
